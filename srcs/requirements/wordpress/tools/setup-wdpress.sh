65e198ee352705b56d9ab41d0ecbbc855618445f
gde-alme <goncalopes98@gmail.com>
2023-08-31 20:19:42 +0100

    working

--- srcs/requirements/wordpress/tools/setup-wdpress.sh
+++ srcs/requirements/wordpress/tools/setup-wdpress.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+echo "---------------------------Starting wordpress setup script---------------------------"
+
+# wait for database to be ready and available
+while true; do
+	is_db_available=$(echo "EXIT" | mariadb -h${DB_HOST} -u${DB_USER} -p${DB_PASS} ${DB_NAME} 2>&1)
+	if [[ -z "$is_db_available" ]]; then
+		break
+	else
+		echo "Waiting for db to be available"
+		sleep 3
+	fi
+done
+
+# add www.conf
 cat > etc/php/7.4/fpm/pool.d/www.conf << EOF
 [www]
 user = www-data
@@ -10,19 +26,13 @@ pm.min_spare_servers = 2
 pm.max_spare_servers = 10
 EOF
 
+# create directories and files
 mkdir -p /var/www
-
-#wget https://wordpress.org/latest.zip
-#unzip latest.zip
-#rm latest.zip
-#mv /wordpress/* /var/www/
-#rm -rf wordpress
-
 #sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
-
 mkdir -p /run/php/;
 touch /run/php/php7.4-fpm.pid;
 
+# if no wp-config.php present create and install wordpress
 if [ ! -f /var/www/wp-config.php ]; then
 	cd /var/www/
 	touch /var/www/wp-config.php
@@ -45,8 +55,18 @@ if [ ! -f /var/www/wp-config.php ]; then
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 	chmod +x wp-cli.phar; 
 	mv wp-cli.phar /usr/local/sbin/wp;
+
+	echo "Downloading wordpress"
 	wp core download --allow-root;
-	wp core install --allow-root --url=$SERVER_NAME --title=$DB_NAME --admin_user=${DB_ROOT_USER} --admin_email=mail@mail.com #--admin_password=${DB_PASS} 
+	chown -R www-data ./*
+	chmod -R 775 ./*
+
+	echo "Installing wordpress"
+	wp core install --allow-root --url=$SERVER_NAME --title=$DB_NAME --admin_user=${DB_ROOT_USER} --admin_password=${DB_PASS} --admin_email=mail@mail.com #--admin_password=${DB_PASS} 
 	#wp user create --allow-root ${DB_ROOT_USER} mail@mail.com --user_pass=${DB_ROOT_PASS};
 fi
 
+
+echo "---------------------------End wordpress setup script---------------------------"
+
+php-fpm7.4 -F
--- srcs/run.sh
+++ srcs/run.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-clear
-
-
-if [ "${1}" == "all" ]; then
-	chmod +x ./requirements/tools/setup-ssl.sh ; bash ./requirements/tools/setup-ssl.sh
-	mkdir -p /home/${USER}/data/wordpress ; chmod -R 777 /home/${USER}/data/wordpress
-	mkdir -p /home/${USER}/data/mysql ; chmod -R 777 /home/${USER}/data/mysql
-	docker-compose -f ./docker-compose.yml up -d
-
-elif [ "${1}" == "build" ]; then
-	mkdir -p /home/${USER}/data/wordpress ; chmod 777 /home/${USER}/data/wordpress
-	mkdir -p /home/${USER}/data/mariadb ; chmod 777 /home/${USER}/data/wordpress
-	docker-compose -f ./docker-compose.yml up -d --build
-
-elif [ "${1}" == "down" ]; then
-	docker-compose -f ./docker-compose.yml down
-
-elif [ "${1}" == "clean" ]; then
-	docker system prune -a -f
-
-elif [ "${1}" == "fclean" ]; then
+if [ "${1}" == "fclean" ]; then
 	rm -rf ./requirements/nginx/tools/*
 	docker stop $(docker ps -qa)
 	docker system prune --all --force --volumes
@@ -28,5 +8,4 @@ elif [ "${1}" == "fclean" ]; then
 	docker volume prune --force
 	docker volume rm $(docker volume ls -q | awk '{printf "%s ", $0}')
 	rm -rf /home/${USER}/data/
-
 fi
