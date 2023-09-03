274fbd3774cfc2f2945a9a4027525a804e52e4ed
gde-alme <gde-alme@debianvm.gde-alme.42.fr>
2023-09-03 18:29:45 +0100

    ready for deliver

--- srcs/requirements/tools/setup-ssl.sh
+++ srcs/requirements/tools/setup-ssl.sh
@@ -6,13 +6,11 @@ sudo apt-get update -y > /dev/null 2>&1; sudo apt-get install -y wget curl libns
 
 wget -q $(curl -s https://api.github.com/repos/FiloSottile/mkcert/releases/latest | grep browser_download_url  | grep linux-amd64 | cut -d '"' -f 4) -O mkcert > /dev/null 2>&1
 
-mv mkcert-v*-linux-amd64 mkcert
-
 chmod a+x mkcert 
 
 sudo mv mkcert /usr/local/bin/
 
-sudo sed -i "1i 127.0.0.1\t${USER}.42.fr" /etc/hosts
+grep -Fxq "127.0.0.0.1\t${USER}.42.fr" /etc/hosts || sudo sed -i "1i 127.0.0.1\t${USER}.42.fr" /etc/hosts
 
 mkcert -install $USER.42.fr > /dev/null 2>&1
 
